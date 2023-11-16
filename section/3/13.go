c835ffbf8124a849f250292635e6ece63c2dac51
Marius Olsen <mariusolsen@outlook.com>
2023-11-16 12:58:20 +0100

    Hands on exercise #1

---
+++ section/3/13.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	// print text with emojis
+	fmt.Println("Hello, world! 🌎")
+
+	fmt.Println(`Hello, world! 🌎
+	My name is Marius, and I am 31 years old.
+	This is a string literal. It can span multiple lines.
+	`)
+
+}
