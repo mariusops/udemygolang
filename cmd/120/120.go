f279043da0c26ccc192d452a52c1d27ba74f294b
Marius Olsen <mariusolsen@outlook.com>
2024-02-01 21:03:52 +0100

    Using make to solve 120 handson map

--- cmd/120/120.go
+++ cmd/120/120.go
@@ -5,11 +5,19 @@ import (
 )
 
 func main() {
-	charInterests := map[string][]string{
-		"bond_james":       {"shaken, not stirred"},
-		"moneypenny_jenny": {"intelligence", "literature", "computer science"},
-		"no_dr":            {"cats", "ice cream", "sunsets"},
-	}
+	charInterests := make(map[string][]string)
+
+	charInterests["bond_james"] = []string{"shaken, not stirred"}
+	charInterests["moneypenny_jenny"] = []string{"intelligence", "literature", "computer science"}
+	charInterests["no_dr"] = []string{"cats", "ice cream", "sunsets"}
+
+	/*
+		charInterests := map[string][]string{
+			"bond_james":       {"shaken, not stirred"},
+			"moneypenny_jenny": {"intelligence", "literature", "computer science"},
+			"no_dr":            {"cats", "ice cream", "sunsets"},
+		}
+	*/
 
 	for character, interests := range charInterests {
 		fmt.Println(character)
