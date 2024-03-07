27535859ddc7fd6fc54a8a88ee77ff9f805458c6
Marius Olsen <mariusolsen@outlook.com>
2024-03-07 12:10:29 +0100

    Remove defer from wrong file

--- cmd/154/154.go
+++ cmd/154/154.go
@@ -5,7 +5,7 @@ package main
 import "fmt"
 
 func main() {
-	defer fmt.Println(foo(2, 2, 3, 4, 5, 6, 7, 8, 9))
+	fmt.Println(foo(1, 2, 3, 4, 5, 6, 7, 8, 9))
 	fmt.Println(bar([]int{1, 2, 3}, []int{4, 5, 6}, []int{7, 8, 9}))
 }
 
