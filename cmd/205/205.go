befab0bbb9a8a4062a0ec9d9db6750e5bea30627
Marius Olsen <mariusolsen@outlook.com>
2024-06-27 13:52:49 +0200

    Mutex

---
+++ cmd/205/205.go
@@ -0,0 +1,50 @@
+/*
+Mutex
+
+A “mutex” is a mutual exclusion lock.
+Mutexes allow us to lock our code, so that only one
+goroutine can access that locked chunk of code at a time.
+
+You can find more information about mutexes in the official documentation:
+https://golang.org/pkg/sync/#Mutex
+
+We use mutex to prevent race conditions.
+*/
+
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+func main() {
+	fmt.Println("CPUs:", runtime.NumCPU())
+	fmt.Println("Goroutines:", runtime.NumGoroutine())
+
+	counter := 0
+
+	const gs = 100
+	var wg sync.WaitGroup
+	wg.Add(gs)
+
+	var mu sync.Mutex
+
+	for i := 0; i < gs; i++ {
+		go func() {
+			mu.Lock()
+			v := counter
+			// time.Sleep(time.Second)
+			runtime.Gosched()
+			v++
+			counter = v
+			mu.Unlock()
+			wg.Done()
+		}()
+		fmt.Println("Goroutines:", runtime.NumGoroutine())
+	}
+	wg.Wait()
+	fmt.Println("Goroutines:", runtime.NumGoroutine())
+	fmt.Println("count:", counter)
+}
